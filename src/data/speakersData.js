51afa3087b06b21619971f1a0ad7f036d6252626
Evandro Pires da Silva <evandro@evandropires.com.br>
2024-09-09 21:56:17 -0300

    Palestrantes confirmados

--- src/data/speakersData.js
+++ src/data/speakersData.js
@@ -1,10 +1,57 @@
 export const speakers = [
-  // {
-  //   name: "Ryan Jones",
-  //   title: "Founder @ Serverless Guru",
-  //   company: "Serverless Guru",
-  //   image: "/ryan-jones-photo.jpeg",
-  //   linkedin: "https://www.linkedin.com/in/ryanjonesirl/"
-  // }
+  {
+    name: "Gabriel Prando",
+    title: "Senior Platform Engineer",
+    company: "iFood",
+    image: "../assets/img/foto-gabriel.jpeg",
+    linkedin: "https://www.linkedin.com/in/prandogabriel/",
+    companyLogo: "../assets/img/logo-ifood.jpeg",
+    companyUrl: "https://www.ifood.com.br/"
+  },
+  {
+    name: "Léo Igor Nunes de Oliveira",
+    title: "Data Engineer",
+    company: "ConectCar",
+    image: "../assets/img/foto-leo.jpeg",
+    linkedin: "https://www.linkedin.com/in/leoigornunes/",
+    companyLogo: "../assets/img/logo-conectcar.jpeg",
+    companyUrl: "https://conectcar.com/"
+  },
+  {
+    name: "Luiz Correia",
+    title: "Sr. Specialist Solutions Architect",
+    company: "Smiles",
+    image: "../assets/img/foto-luiz.jpeg",
+    linkedin: "https://www.linkedin.com/in/luizgcorreia/",
+    companyLogo: "../assets/img/logo-smiles.jpeg",
+    companyUrl: "https://smiles.com.br/"
+  },
+  {
+    name: "Lucas Andrade Cioffi",
+    title: "Security Researcher",
+    company: "Tenchi Security",
+    image: "../assets/img/foto-lucas.jpeg",
+    linkedin: "https://www.linkedin.com/in/lacioffi/",
+    companyLogo: "../assets/img/logo-tenchi.jpeg",
+    companyUrl: "https://www.tenchisecurity.com.br/"
+  },
+  {
+    name: "Peterson Larentis",
+    title: "Senior Specialist Solutions Architect, Serverless - LATAM",
+    company: "AWS",
+    image: "../assets/img/foto-peterson.jpeg",
+    linkedin: "https://www.linkedin.com/in/peterson-larentis/",
+    companyLogo: "../assets/img/logo-aws.jpeg",
+    companyUrl: "https://aws.amazon.com/"
+  },
+  {
+    name: "Willian Peixoto",
+    title: "Staff Engineer",
+    company: "Serverless Guru",
+    image: "../assets/img/foto-willian.jpeg",
+    linkedin: "https://www.linkedin.com/in/peixotowill/",
+    companyLogo: "../assets/img/logo-serverlessguru.jpeg",
+    companyUrl: "https://www.serverlessguru.com/"
+  }
 ];
 export const cfpAvailable = true;
