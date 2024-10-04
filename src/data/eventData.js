6072b471e60538e5c613809fa954041aaed27b16
Evandro Pires da Silva <evandro@evandropires.com.br>
2024-10-03 22:41:00 -0300

    Grade de palestras

--- src/data/eventData.js
+++ src/data/eventData.js
@@ -1,25 +1,23 @@
-export const eventDetails = {}
+// export const eventDetails = {}
 
-// export const eventDetails = {
-//   title: "Agenda",
-//   date: "19 de Outubro",
-//   location: "São Paulo",
-//   schedule: [
-//     { time: '08:00', title: 'Credenciamento', speaker: '-' },
-//     { time: '09:00', title: 'Abertura', speaker: 'Evandro Pires @Sem Servidor Podcast' },
-//     { time: '09:15', title: '', speaker: 'Ben Pollard @ aleios' },
-//     { time: '09:40', title: 'Maximizando a Observabilidade em Arquiteturas Serverless', speaker: 'Tiago Krebs @ Azion' },
-//     { time: '10:05', title: 'Increase the security of your applications by using an One-Time Password (OTP) serverless solution', speaker: 'Paulo Aragão @ AWS' },
-//     { time: '10:30', title: 'Networking + Coffee', speaker: '-' },
-//     { time: '11:00', title: 'É possivel trabalhar com BigData utilizando serverless?', speaker: 'Alexandre Tavares @ Cogna Educação' },
-//     { time: '11:25', title: 'Serverless: quando usar e quando não usar?', speaker: 'Bruno Carneiro @ Base39 e Alan Araya @ Grupo Regazzo' },
-//     { time: '12:05', title: 'Almoço e networking', speaker: '-' },
-//     { time: '13:35', title: 'Desvendando os desafios de construir uma plataforma serverless no Edge', speaker: 'Ricardo Moreira @ Azion' },
-//     { time: '14:15', title: 'Padrões avançados de fluxos de trabalho serverless com AWS Step Functions', speaker: 'Bianca Mota e Letícias Dornelas @ AWS' },
-//     { time: '14:55', title: 'Acelerando a Interoperabilidade em Healthcare com Serverless', speaker: 'Roberto Alves e Marcos Vinicius @ Hospital Albert Einstein' },
-//     { time: '15:35', title: 'Networking + Coffee', speaker: '-' },
-//     { time: '16:35', title: 'Serverless para Computação de Alta Performance (HPC)', speaker: 'Guilherme Dalla Rosa @ MerCloud' },
-//     { time: '17:15', title: 'Serverless Cost Savings', speaker: 'Ryan Jones @ Serverless Guru' },
-//     { time: '18:05', title: 'Encerramento', speaker: 'Evandro Pires @ Sem Servidor Podcast' }
-//   ]
-// };
+export const eventDetails = {
+  title: "Agenda",
+  date: "19 de Outubro",
+  location: "São Paulo",
+  schedule: [
+    { time: '09h00', title: 'Credenciamento', speaker: '-' },
+    { time: '10h00', title: 'Abertura', speaker: 'Evandro Pires - AWS Serverless Hero' },
+    { time: '10h15', title: 'As misconfigurations mais comuns e mais perigosas em ambientes serverless', speaker: 'Lucas Cioffi @ Tenchi Security' },
+    { time: '10h50', title: 'Construindo sua própria layer de observabilidade com AWS Lambda telemetry API', speaker: 'Gabriel Prando @ iFood' },
+    { time: '11h25', title: 'Melhorando Resiliência e Cortando Custos na Ingestão de Dados', speaker: 'Leo Igor Nunes de Oliveira @ ConectCar' },
+    { time: '12h00', title: 'Almoço incluso + Networking', speaker: '-' },
+    { time: '13h00', title: 'Containers or Serverless Functions - When to choose what?', speaker: 'Peterson Larentis @ AWS' },
+    { time: '13h35', title: 'Saga Pattern com Step Functions', speaker: 'Willian Peixoto @ Serverless Guru' },
+    { time: '14h10', title: 'Descomplicando 12 factor apps com serverless em AWS', speaker: 'Gabriel Oswaldo @ CI&T + Caio Pardal @ GoCo.io, Inc.' },
+    { time: '14h45', title: 'Coffee Break + Networking', speaker: '-' },
+    { time: '15:45', title: 'AIOps com serverless', speaker: 'Henrique Ferro @ Oracle' },
+    { time: '16h20', title: 'Em breve', speaker: '-' },
+    { time: '16h55', title: 'Serverless-first na Smiles: aprimorando a experiência do cliente', speaker: 'Luiz Correia @ Smiles' },
+    { time: '17h30', title: 'Encerramento', speaker: 'Evandro Pires - AWS Serverless Hero' }
+  ]
+};
