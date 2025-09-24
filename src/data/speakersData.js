d7a17b75a704a9f8e6b72063b028794fe35d3821
Evandro Pires <evandro@evandropires.com.br>
2025-09-24 15:19:10 -0300

    Primeiros palestrantes

--- src/data/speakersData.js
+++ src/data/speakersData.js
@@ -1,93 +1,43 @@
 export const speakers = [
   {
-    name: "Gabriel Prando",
-    title: "Senior Platform Engineer",
-    company: "iFood",
-    image: "../assets/img/foto-gabriel.jpeg",
-    linkedin: "https://www.linkedin.com/in/prandogabriel/",
-    companyLogo: "../assets/img/logo-ifood.jpeg",
-    companyUrl: "https://www.ifood.com.br/"
+    name: "Hazel Saenz",
+    title: "AWS Serverless Hero, Cloud Software Architect",
+    company: "Caylent",
+    image: "../assets/img/foto-hazel.jpeg",
+    linkedin: "https://www.linkedin.com/in/hazelsaenz/",
+    companyLogo: "../assets/img/logo-caylent.jpeg",
+    companyUrl: "https://caylent.com",
+    talkTitle: "From Chaos to Clarity: Choosing the Right Stack for GenAI on AWS"
   },
   {
-    name: "Léo Igor Nunes de Oliveira",
-    title: "Data Engineer",
-    company: "ConectCar",
-    image: "../assets/img/foto-leo.jpeg",
-    linkedin: "https://www.linkedin.com/in/leoigornunes/",
-    companyLogo: "../assets/img/logo-conectcar.jpeg",
-    companyUrl: "https://conectcar.com/"
+    name: "Ivan Rodolpho",
+    title: "Software Engineer Specialist",
+    company: "Itaú",
+    image: "../assets/img/foto-ivan.jpeg",
+    linkedin: "https://www.linkedin.com/in/ivanrodolpho/",
+    companyLogo: "../assets/img/logo-itau.jpeg",
+    companyUrl: "https://www.itau.com.br",
+    talkTitle: "Revolução Serverless: Como o Itaú Unibanco modernizou seu core bancário internacional"
   },
   {
-    name: "Luiz Correia",
-    title: "Sr. Specialist Solutions Architect",
-    company: "Smiles",
-    image: "../assets/img/foto-luiz.jpeg",
-    linkedin: "https://www.linkedin.com/in/luizgcorreia/",
-    companyLogo: "../assets/img/logo-smiles.jpeg",
-    companyUrl: "https://smiles.com.br/"
-  },
-  {
-    name: "Lucas Andrade Cioffi",
-    title: "Security Researcher",
-    company: "Tenchi Security",
-    image: "../assets/img/foto-lucas.jpeg",
-    linkedin: "https://www.linkedin.com/in/lacioffi/",
-    companyLogo: "../assets/img/logo-tenchi.jpeg",
-    companyUrl: "https://www.tenchisecurity.com.br/"
-  },
-  {
-    name: "Peterson Larentis",
-    title: "Senior Specialist Solutions Architect, Serverless - LATAM",
-    company: "AWS",
-    image: "../assets/img/foto-peterson.jpeg",
-    linkedin: "https://www.linkedin.com/in/peterson-larentis/",
-    companyLogo: "../assets/img/logo-aws.jpeg",
-    companyUrl: "https://aws.amazon.com/"
-  },
-  {
-    name: "Willian Peixoto",
-    title: "Staff Engineer",
-    company: "Serverless Guru",
-    image: "../assets/img/foto-willian.jpeg",
-    linkedin: "https://www.linkedin.com/in/peixotowill/",
-    companyLogo: "../assets/img/logo-serverlessguru.jpeg",
-    companyUrl: "https://www.serverlessguru.com/"
-  },
-  {
-    name: "Henrique Ferro",
-    title: "Director of Infra Engineering & Native Brazil",
-    company: "Oracle",
-    image: "../assets/img/foto-henrique.jpeg",
-    linkedin: "https://www.linkedin.com/in/henriqueferro/",
-    companyLogo: "../assets/img/logo-oracle.jpeg",
-    companyUrl: "https://www.oracle.com/"
-  },
-  {
-    name: "Gabriel Oswaldo",
-    title: "Backend Software Engineer",
-    company: "CI&T",
-    image: "../assets/img/foto-oswaldo.jpeg",
-    linkedin: "https://www.linkedin.com/in/gabrieloswaldo/",
-    companyLogo: "../assets/img/logo-ciet.jpeg",
-    companyUrl: "https://ciandt.com/br/"
-  },
-  {
-    name: "Caio Pardal",
+    name: "Lucas Machado",
     title: "Software Engineer",
-    company: "GoCo.io, Inc.",
-    image: "../assets/img/foto-caio.jpeg",
-    linkedin: "https://www.linkedin.com/in/henriqueferro/",
-    companyLogo: "../assets/img/logo-goco.jpeg",
-    companyUrl: "https://www.goco.io/"
+    company: "Conta Simples",
+    image: "../assets/img/foto-lucas.jpeg",
+    linkedin: "https://www.linkedin.com/in/lucasmachado-br/",
+    companyLogo: "../assets/img/logo-contasimples.jpeg",
+    companyUrl: "https://contasimples.com/",
+    talkTitle: "De fintech em crescimento a plataforma escalável: como a Conta Simples constrói seu core em arquitetura serverless"
   },
   {
-    name: "Marcos Carvalho",
-    title: "Security Architect",
-    company: "Azion",
-    image: "../assets/img/foto-marcos.jpeg",
-    linkedin: "https://www.linkedin.com/in/marcos-rodrigues-de-carvalho-8ab2381b7/",
-    companyLogo: "../assets/img/logo-azion.jpeg",
-    companyUrl: "https://azion.com/"
+    name: "Ricardo Marques",
+    title: "Senior AppMod and Serverless Specialist",
+    company: "AWS",
+    image: "../assets/img/foto-ricardo.jpeg",
+    linkedin: "https://www.linkedin.com/in/ricardo-marques-45846425/",
+    companyLogo: "../assets/img/logo-aws.jpeg",
+    companyUrl: "https://aws.amazon.com/",
+    talkTitle: "Potencializando IAs Agênticas com Servidores MCP Serverless"
   }
 ];
 export const cfpAvailable = true;
